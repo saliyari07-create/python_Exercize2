9f3116f2c503bf8e30d4ba7c8f32be761bd7f41d
saliyari07-create <saliyari07@gmail.com>
2025-11-14 09:49:05 +0330

    Add files via upload

---
+++ exercize9.py
@@ -0,0 +1,9 @@
+while True:
+ x=int(input("enter first number:\n"))
+ y=int(input("enter second number:\n"))
+ z=int(input("enter third number:\n"))
+ m=int(input("enter fourth number:\n"))
+ p=float(input("enter n  number:\n")) 
+ s= input("یک عدد وارد کنید (برای پایان 0 یا 01 بزنید): ")
+ print(max(x,y,z,m,p))
+ print(x+y+z+m+p)
