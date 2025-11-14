d30235fc42bc29e893ec1aff48fd8480bcc3b8b7
saliyari07-create <saliyari07@gmail.com>
2025-11-14 14:12:31 +0330

    Add files via upload

---
+++ exercize.py
@@ -0,0 +1,14 @@
+students=['sara','neda','ali','sahar','hamed']
+students.append('leila')
+print(students)
+students.pop(2)
+print(students) 
+students[2:3]=['narges','fateme']
+print(students)
+for ali in students:
+  if 'ali'==students: 
+   print('exist')
+else:
+    print('no exist')
+print(students)
+
